411268a167a600b2f8971d43c1ddff48845141f5
Alexis Vighi <alexis.vighi@yahoo.fr>
2021-01-14 12:05:20 +0100

    Ajout des fonctions de preprocessing qui ont ete cree lors du dernier tp

---
+++ preprocessing.py
@@ -0,0 +1,37 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd
+
+def tok(doc, sw=False):
+    result = []
+    for line in doc:
+        tokenized = nltk.word_tokenize(line)
+        if sw:
+            stop_words = set(stopwords.words('english'))
+            l = [word for word in tokenized if word not in stop_words and word.isalpha()]
+            result.append(l)
+        else:
+            l = [word for word in tokenized]
+            result.append(l)
+    return result
+
+
+def bow(doc, vectorizer=None):
+    doc = tok(doc, sw=True)
+    porter = PorterStemmer()
+    docs = []
+    for d in doc:
+        li = []
+        for word in d:
+            li.append(porter.stem(word))
+        docs.append(" ".join(li))
+    if vectorizer is None:
+        vectorizer = CountVectorizer()
+        bow = vectorizer.fit_transform(docs)
+    else:
+        bow = vectorizer.transform(docs)
+    bow = bow.toarray()
+    bow = pd.DataFrame(bow)
+    return bow, vectorizer
